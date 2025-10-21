5ef24bdd16a066f92145e8a3c44073f46542c38a
Your Name <your.email@example.com>
2025-10-21 19:06:34 +0700

    Fix: correct test structure and single passing test

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnSentData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные
+        // Выполняемые действия
+        .when()
+                .post("/post")
+        // Проверки
+        .then()
+                .statusCode(200)
+                // Проверяем, что в поле data вернулись отправленные данные
+                .body("data", equalTo("some data"));
+    }
+}
